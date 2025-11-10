59a44e6db6977e5ffc0ed67e097ecb45264083b0
Milton Cruz <mr.cruzm@gmail.com>
2025-11-10 09:16:54 -0800

    Added About and Home components, installed React Router and setup in App file

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 import Login from './components/Login';
+import About from './components/About';
+import Weather from './components/Weather';
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{' '}
+        <Link to="/login">Login</Link> | <Link to="/weather">Weather</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/weather" element={<Weather />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
