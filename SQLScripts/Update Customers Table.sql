13361d3f5a80346966a2038dfff5d4130639cb58
Milton Cruz <mr.cruzm@gmail.com>
2025-11-13 14:38:41 -0800

    Adding/Updating SQL scripts

--- SQLScripts/Update Customers Table.sql
+++ SQLScripts/Update Customers Table.sql
@@ -1,14 +1,14 @@
 USE [MyGuitarShop]
 GO
 
-ALTER TABLE [dbo].[Products]
+ALTER TABLE [dbo].[Customers]
 ADD 
-    [IsActive] [bit] NULL CONSTRAINT DF_Products_IsActive DEFAULT ((1)), -- Add IsActive column with default constraint
+    [IsActive] [bit] NULL CONSTRAINT DF_Customers_IsActive DEFAULT ((1)), -- Add IsActive column with default constraint
     [DateCreated] [datetime] NULL; -- Add DateCreated column
     [DateUpdated] [datetime] NULL; -- Add DateUpdated column
 GO
 
 
-UPDATE [dbo].[Products]
+UPDATE [dbo].[Customers]
    SET [IsActive] = 1
 GO
